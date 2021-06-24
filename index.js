4e60820ce499912416a45427f859d3cddd918cc0
MINH NGUYEN HUY <huyminh1115@gmail.com>
2021-06-24 12:21:02 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function () {
-    const second = 1000,
+    const second = 1,
           minute = second * 60,
           hour = minute * 60,
           day = hour * 24;
@@ -11,30 +11,22 @@
     //     birthday = data.date;
     //     console.log(birthday);
     // });
-    let birthday = "Sep 30, 2021 00:00:00",
-    countDown = new Date(birthday).getTime(),
-    x = setInterval(function() {    
-
-        let now = new Date().getTime(),
-        distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-        let headline = document.getElementById("headline"),
-            countdown = document.getElementById("countdown"),
-            content = document.getElementById("content");
-
-        headline.innerText = "It's my birthday!";
-        countdown.style.display = "none";
-        content.style.display = "block";
-
-        clearInterval(x);
+    let distance = 10;
+    setInterval(function() {
+        distance--;
+        if(distance < 0){
+            _distance = -distance;
+            document.getElementById("days").innerText = -Math.floor(_distance / (day)),
+            document.getElementById("hours").innerText = -Math.floor((_distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = -Math.floor((_distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = -Math.floor((_distance % (minute)) / second);
         }
-        //seconds
-    }, 0)
+        else{
+            _distance = distance;
+            document.getElementById("days").innerText = Math.floor(_distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((_distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((_distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((_distance % (minute)) / second);
+        }
+    }, 1000)
 }());
