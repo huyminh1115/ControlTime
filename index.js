a65333c5dd2b0032ad7ade771fbc7f2bbd158c25
MINH NGUYEN HUY <huyminh1115@gmail.com>
2021-06-24 16:46:50 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+distance = 0;
+dk = true;
+number = 3;
 (function () {
     const second = 1,
           minute = second * 60,
@@ -11,9 +14,12 @@
     //     birthday = data.date;
     //     console.log(birthday);
     // });
-    let distance = 10;
     setInterval(function() {
-        distance--;
+        if(dk == true){
+            distance = distance + 1/number;
+        }else{
+            distance = distance - 1 ;
+        }
         if(distance < 0){
             _distance = -distance;
             document.getElementById("days").innerText = -Math.floor(_distance / (day)),
@@ -30,3 +36,13 @@
         }
     }, 1000)
 }());
+
+function change1(){
+    dk = true;
+    console.log(dk);
+}
+
+function change2(){
+    dk = false;
+    console.log(dk);
+}
